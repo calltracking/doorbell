2758252b5c2007f1b31ca20015beab62ff9102ed
Jeremy Curcio <jeremy.curcio@calltrackingmetrics.com>
2018-03-20 11:14:42 -0400

    Initial Commit

---
+++ doorbell/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  doorbell
+//
+//  Created by Jeremy Curcio on 3/20/18.
+//  Copyright © 2018 Jeremy Curcio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ doorbell/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  doorbell
+//
+//  Created by Jeremy Curcio on 3/20/18.
+//  Copyright © 2018 Jeremy Curcio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
