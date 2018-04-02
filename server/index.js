641e0b62574b07b6140ca65e617346c99e26dda3
Jeremy Curcio <jeremy1026@gmail.com>
2018-04-02 13:02:43 -0400

    Log number of connected clients.

--- server/index.js
+++ server/index.js
@@ -5,10 +5,12 @@ const WebSocket = require('ws');
  
 const wss = new WebSocket.Server({ port: 3030 });
 var _ws;
+var clients = [];
 
 wss.on('connection', function connection(ws, req) {
   _ws = ws;
   const ip = req.connection.remoteAddress;
+  clients.push(ip);
   console.log('connection from '+ip);
   _ws.on('message', function incoming(message) {
     console.log('received: %s', message);
@@ -17,9 +19,13 @@ wss.on('connection', function connection(ws, req) {
     }
   });
   _ws.on('close', function closed() {
-  	console.log('client disconnect '+ip);
+    console.log('client disconnect '+ip);
+    var index = clients.indexOf(ip);
+    if (index > -1) {
+      clients.splice(index, 1);
+    }
   });
- 
+  console.log(clients.length + " Clients Connected");
 });
 
 function broadcast(data) {
@@ -31,19 +37,19 @@ function broadcast(data) {
 };
 
 buttons = [{"location": "207", "address": "34:d2:70:0d:9a:a5"}, 
-					 {"location": "207 B", "address": "88:71:e5:45:dc:f3"}, 
-					 {"location": "308", "address": "68:37:e9:f2:c3:8c"}];
+           {"location": "207 B", "address": "88:71:e5:45:dc:f3"}, 
+           {"location": "308", "address": "68:37:e9:f2:c3:8c"}];
 
 buttons.forEach(function(element) {
-	
-	console.log(element.location+": "+element.address);
-	const DASH_BUTTON_MAC_ADDRESS = element.address;
-	 
-	let button = new DashButton(DASH_BUTTON_MAC_ADDRESS);
+  
+  console.log(element.location+": "+element.address);
+  const DASH_BUTTON_MAC_ADDRESS = element.address;
+   
+  let button = new DashButton(DASH_BUTTON_MAC_ADDRESS);
 
-	let subscription = button.addListener(async () => {
-		console.log('[' + new Date().toUTCString() + '] Button Pressed');
-		broadcast('{"type":"button pressed", "location":"'+element.location+'"}');
-	});
+  let subscription = button.addListener(async () => {
+    console.log('[' + new Date().toUTCString() + '] Button Pressed');
+    broadcast('{"type":"button pressed", "location":"'+element.location+'"}');
+  });
 
 });
